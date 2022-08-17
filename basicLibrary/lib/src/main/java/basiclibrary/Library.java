6ff3bdb260972d19558771bd5ffe9bbd946abf5e
Lucy Gelderloos <lucygel@gmail.com>
2022-08-16 19:25:15 -0700

    All methods added; all tests pass

--- basicLibrary/lib/src/main/java/basiclibrary/Library.java
+++ basicLibrary/lib/src/main/java/basiclibrary/Library.java
@@ -1,19 +1,50 @@
-import java.util.Random;
-
 package basiclibrary;
 
+import java.util.Random;
 public class Library {
+    public boolean someLibraryMethod() {return true;}
 
     public static int[] roll(int n) {
-        int[] rolls = int[n];
+        int[] rolls = new int[n];
         for(int i = 0; i < n; i++){
             Random random = new Random();
-            int thisRoll = (random(int 6)) + 1;
+            int thisRoll = (random.nextInt(6)) + 1;
             rolls[i] = thisRoll;
         }
         return rolls;
     }
 
-    int numRolls = 10;
-    roll(numRolls);
+    public static boolean containsDuplicates(int[] array) {
+        for(int i = 0; i < array.length; i++) {
+            for(int j = i + 1; j < array.length; j++){
+                if(array[i] == array[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static float findAverage(int[] array) {
+        float total = 0.0f;
+        for(int i = 0; i < array.length; i++){
+            total = total + array[i];
+        }
+        return total/array.length;
+    }
+
+    public static int[] findLowestAverage(int[][] matrix) {
+        float lowestAverage = findAverage(matrix[0]);
+        int lowestAverageIndex = 0;
+        for (int i = 1; i < matrix.length; i++) {
+            float thisAverage = findAverage(matrix[i]);
+            if (thisAverage <= lowestAverage) {
+                lowestAverage = thisAverage;
+                lowestAverageIndex = i;
+            }
+        }
+        return matrix[lowestAverageIndex];
+    }
 }
+
+
